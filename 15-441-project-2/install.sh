932c9ba5fb1fcc0eb43f5e8c3ffa8e61c24ac24f
Zecheng He <zechengh@andrew.cmu.edu>
2020-10-24 02:35:59 -0400

    Add install script

---
+++ 15-441-project-2/install.sh
@@ -0,0 +1,16 @@
+sudo apt-get install -y git gdb valgrind python-dev libffi-dev libssl-dev
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install tshark
+sudo pip install --upgrade pip
+sudo pip install tcconfig
+sudo pip install scapy
+sudo pip install pytest==4.6.5
+sudo pip install fabric
+sudo pip install typing
+sudo pip install cryptography
+sudo pip3 install --upgrade pip
+sudo pip3 install scapy
+sudo pip3 install matplotlib
+pip3 install pytest
+pip3 install fabric
+sudo apt-get install -y python-tk
+sudo apt install -y clang-format
